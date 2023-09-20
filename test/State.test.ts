c0e6c47df48fc8754edaabbd5547b115d096816b
Sami Kashbi <sami.kashbi@microfocus.com>
2023-09-20 13:39:49 +0300

    Added basic uses cases

--- test/State.test.ts
+++ test/State.test.ts
@@ -4,6 +4,23 @@ describe('State class', () => {
   it('should add transitions correctly', () => {
     const state = new State('A');
     state.addTransition('event1', 'B');
-    expect(state.getNextStates('event1')).toEqual(['B']);
+    let nextStates = state.getNextStates('event1');
+    expect(Array.isArray(nextStates)).toEqual(true);
+    expect(nextStates[0].targetState).toEqual('B');
   });
+
+  it('should report invalid transitions ', () => {
+    const state = new State('A');
+    state.addTransition('event1', 'B');
+    let nextStates = state.getNextStates('xxxxx');
+    expect(nextStates.length).toEqual(0);
+  });
+
+  it('should report invalid transitions since not defined ', () => {
+    const state = new State('A');
+    let nextStates = state.getNextStates('event5');
+    expect(nextStates.length).toEqual(0);
+  });
+
+
 });
